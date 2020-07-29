424a9d2ac4c90344897752d9ae34340aa4e694ef
Marius van Niekerk <marius.v.niekerk@gmail.com>
2020-07-29 16:46:49 -0400

    Upgrade minimum version of monkeytype

--- pytest_monkeytype/plugin.py
+++ pytest_monkeytype/plugin.py
@@ -16,16 +16,17 @@ class MonkeyTypePlugin(object):
     A pytest plugin that profiles function calls to extract type info.
     """
 
-    def __init__(self):
+    def __init__(self, typed_dict_size=0):
         """Create a new PyAnnotatePlugin that analyzes function calls to extract type info.
 
         Initialize the plugin that profiles calls to extract type info.
         """
         from monkeytype.config import DefaultConfig
 
+        self.typed_dict_size = typed_dict_size
         self.config = DefaultConfig()
         self.trace_logger = self.config.trace_logger()
-        self.tracer = None  # type: Optional[CallTracer]
+        self.tracer: "Optional[CallTracer]" = None
 
     def pytest_collection_finish(self, session):
         """Configure MonkeyType and set as profiler after collection.
@@ -36,20 +37,13 @@ class MonkeyTypePlugin(object):
         """
         from monkeytype.tracing import CallTracer
 
-        try:
-            self.tracer = CallTracer(
-                logger=self.trace_logger,
-                code_filter=self.config.code_filter(),
-                sample_rate=None,
-            )
-        except TypeError:
-            # max_typed_dict_size argument added since MonkeyType 20.4.1 or later
-            self.tracer = CallTracer(
-                logger=self.trace_logger,
-                max_typed_dict_size=0,
-                code_filter=self.config.code_filter(),
-                sample_rate=None,
-            )
+        self.tracer = CallTracer(
+            logger=self.trace_logger,
+            max_typed_dict_size=self.typed_dict_size,
+            code_filter=self.config.code_filter(),
+            sample_rate=None,
+        )
+
         sys.setprofile(self.tracer)
 
     def pytest_unconfigure(self, config):
@@ -76,11 +70,20 @@ def pytest_addoption(parser):
             ' "monkeytype.sqlite3"'
         ),
     )
+    parser.addoption(
+        "--monkeytype-max-typed-dict-size",
+        help=(
+            "Maximum number of fields to include in generate TypedDict types"
+        )
+    )
 
 
 def pytest_configure(config):
     """Enable and configure the output location."""
     option_value = config.getoption("--monkeytype-output")
+    typed_dict_size = config.getoption("--monkeytype-max-typed-dict-size") or 0
+    typed_dict_size = int(typed_dict_size)
+
     if option_value:
         os.environ["MT_DB_PATH"] = os.path.abspath(option_value)
-        config.pluginmanager.register(MonkeyTypePlugin())
+        config.pluginmanager.register(MonkeyTypePlugin(typed_dict_size))
